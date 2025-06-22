09100ca41308664dea824a863c44e23f2bbb033b
victorio nogueira <victorionogueira@gmail.com>
2025-06-22 19:45:15 -0300

    feat(user): add createdAt and updatedAt fields

--- src/main/java/com/nogueira/authentication_service/models/User.java
+++ src/main/java/com/nogueira/authentication_service/models/User.java
@@ -1,16 +1,27 @@
 package com.nogueira.authentication_service.models;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.nogueira.authentication_service.enums.RoleEnum;
 import com.nogueira.authentication_service.enums.StatusEnum;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity(name = "users")
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 	
 	@Id
@@ -23,10 +34,17 @@ public class User {
 	private RoleEnum role;
 	@Enumerated(EnumType.STRING)
 	private StatusEnum status;
+	@CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 	
 	public User(){
 	}
-	
+	 
 	public User(String name, String email, String password, RoleEnum role, StatusEnum status) {
 		this.name = name;
 		this.email = email;
@@ -77,6 +95,14 @@ public class User {
 	
 	public void setStatus(StatusEnum status) {
 		this.status = status;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
 	}
 	
 }
