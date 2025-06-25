1e965de1e19a7ae77dc6aa52c3744ff489872bed
victorio nogueira <victorionogueira@gmail.com>
2025-06-24 21:28:22 -0300

    Configura Spring Security com regras de autorização e CORS

---
+++ src/main/java/com/nogueira/authentication_service/security/SecurityConfiguration.java
@@ -0,0 +1,67 @@
+package com.nogueira.authentication_service.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	@Autowired
+	SecurityFilter securityFilter;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.cors(cors -> cors.configurationSource(corsConfigurationSource()))//solução para o erro com CORS
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
+						.anyRequest().authenticated()
+				)
+				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
+	
+	//solução para o erro com CORS
+		@Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.addAllowedOrigin("http://localhost:5173");  // Permite o domínio de origem
+	        config.addAllowedMethod("GET");  // Permite métodos GET
+	        config.addAllowedMethod("POST"); // Permite métodos POST
+	        config.addAllowedMethod("PUT");  // Permite métodos PUT
+	        config.addAllowedMethod("DELETE"); // Permite métodos DELETE
+	        config.addAllowedHeader("*");  // Permite todos os cabeçalhos
+	        config.setAllowCredentials(true); // Permite envio de credenciais (cookies, auth headers)
+
+	        source.registerCorsConfiguration("/**", config); // Aplica a configuração a todas as rotas
+	        return source;
+	    }
+		
+		@Bean
+		public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+			return authenticationConfiguration.getAuthenticationManager();
+		}
+		
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+
+}
