da1a22ab1f7706ac725bea88abaa7a8e0e1ebe4d
victorio nogueira <victorionogueira@gmail.com>
2025-06-24 21:27:03 -0300

    feat(security): adiciona filtro JWT e configurações do Spring Security

---
+++ src/main/java/com/nogueira/authentication_service/security/SecurityFilter.java
@@ -0,0 +1,58 @@
+package com.nogueira.authentication_service.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.nogueira.authentication_service.repositories.UserRepository;
+import com.nogueira.authentication_service.services.TokenService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+	@Autowired
+	private TokenService tokenService;
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		 // Ignora requisições OPTIONS (preflight requests)
+		if("OPTIONS".equalsIgnoreCase(request.getMethod())){
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
+		
+		String token = this.recoverToken(request);
+		if(token != null) {
+			String email = tokenService.validateAccessToken(token);
+			UserDetails user = repository.FindByEmail(email); //ficar atento pois o meu username é o email e eu coloquei find by email
+		
+			Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+	
+	private String recoverToken(HttpServletRequest request) {
+		String header = request.getHeader("Authorization");
+		if(header == null) return null;
+		String token = header.replace("Bearer ", "");
+		return token;
+	}
+
+}
