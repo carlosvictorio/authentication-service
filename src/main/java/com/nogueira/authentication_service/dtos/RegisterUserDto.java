6a6479f4dc57b587a6212d41aead817dbe43ae6d
victorio nogueira <victorionogueira@gmail.com>
2025-07-01 20:28:36 -0300

    refactor(dto): remove role and status from RegisterUserDto, set defaults in service

--- src/main/java/com/nogueira/authentication_service/dtos/RegisterUserDto.java
+++ src/main/java/com/nogueira/authentication_service/dtos/RegisterUserDto.java
@@ -1,15 +1,10 @@
 package com.nogueira.authentication_service.dtos;
 
-import com.nogueira.authentication_service.enums.RoleEnum;
-import com.nogueira.authentication_service.enums.StatusEnum;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterUserDto(	 	
 		@NotBlank String name,
 	    @NotBlank @Email String email,
-	    @NotBlank String password,
-	    @NotBlank RoleEnum role,
-	    @NotBlank StatusEnum status) {
+	    @NotBlank String password) {
 }
