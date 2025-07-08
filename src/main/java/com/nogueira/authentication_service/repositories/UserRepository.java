e1d51d23dc7c3cf5eafaab1c91d2665f342fa9a2
victorio nogueira <victorionogueira@gmail.com>
2025-07-08 14:11:25 -0300

    fix(UserRepository): replaces `findByEmail` return type from UserDetails to User

--- src/main/java/com/nogueira/authentication_service/repositories/UserRepository.java
+++ src/main/java/com/nogueira/authentication_service/repositories/UserRepository.java
@@ -1,12 +1,10 @@
 package com.nogueira.authentication_service.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import com.nogueira.authentication_service.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	UserDetails findByEmail(String email);
-	User findByUsername(String email);
+	User findByEmail(String email);
 	Boolean existsByEmail(String email);
 }
